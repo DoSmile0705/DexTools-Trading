aa2583cb5d4a72a0fa24e65b76c329591bc06520
DevCreator0705 <136826855+DevCreator0705@users.noreply.github.com>
2023-12-19 23:59:15 +0900

    updated-server'

--- api/index.py
+++ api/index.py
@@ -2,17 +2,21 @@ import requests
 from flask import Flask
 from flask import Request
 
+
 app = Flask(__name__)
 
-
 @app.route('/server/login', methods=['POST', 'GET'])
-def get_cred():
-    dataCred = Request.data
-    datatype = type(dataCred)
-    datatypestr = str(datatype)
 
-    if (dataCred):
-        return datatypestr
+def get_cred():
+    data = Request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    init_email = "whaleundercover@gmail.com" 
+    init_pass = "DexTools0705!" 
+
+    if(email == init_email and password == init_pass):
+        return 'okay'
     else:
         return 'sdafffffffffffffffdasfdasfasdfasfsadfasdfafdadf'
     
