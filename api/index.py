1d5771d0cf0ccc7fefd5e414831f7d996f3cbd5d
DevCreator0705 <136826855+DevCreator0705@users.noreply.github.com>
2023-12-20 02:09:30 +0900

    updated

--- api/index.py
+++ api/index.py
@@ -1,15 +1,22 @@
 import requests
 from flask import Flask
-from flask import Request
+from flask import request
 
 
 app = Flask(__name__)
 
 
 @app.route('/server/login', methods=['POST', 'GET'])
-def get_cred(request):
-    # data = Request.get_json()
-    return request.form.data['email']
+def get_cred():
+    dataCred = request.form
+    new_email = dataCred['email']
+    new_pass = dataCred['password']
+    init_email = 'whaleundercover@gmail.com'
+    init_pass = 'DexTools0705!'
+    if (new_email == init_email and new_pass == init_pass):
+        return 'ok'
+    else:
+        return 'Not'         
     # password = data.get('password')
 
     # init_email = "whaleundercover@gmail.com"
